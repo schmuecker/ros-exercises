b956015b2681f3299fdb2ea616b7ca797492b87a
Tobias Schmücker <schmuecker.tobias@gmail.com>
2022-05-02 10:45:37 +0200

    Add util

--- husky_highlevel_controller/include/husky_highlevel_controller/util.py
+++ husky_highlevel_controller/include/husky_highlevel_controller/util.py
@@ -1,7 +1,18 @@
-import random
+from sensor_msgs.msg import LaserScan
 
-def getName():
-    return "Benny"
+def filter_scan(data):
+    ranges_filtered = (v for v in data.ranges if v > data.range_min)
+    min_distance = min(ranges_filtered)
+    min_index = data.ranges.index(min_distance)
+    start_index = min_index - 2
+    no_distances = 5
 
-def getStatus():
-    return random.choice(["happy","awesome"])
+    measurements_5 = data.ranges[start_index : (start_index + no_distances)]
+
+    new_msg = LaserScan()
+    new_msg = data
+    new_msg.angle_min = data.angle_min + (start_index * data.angle_increment)
+    new_msg.angle_max = new_msg.angle_min + (no_distances-1) * data.angle_increment
+    new_msg.ranges = measurements_5
+
+    return new_msg
